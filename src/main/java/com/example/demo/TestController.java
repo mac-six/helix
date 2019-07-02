bfa877d08f5fdde5193bae9111d239cbc83a7083
David MacDonald <davidm@yambay.com>
2019-07-02 15:10:23 +0100

    Add home controller

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author <a href="mailto:davidm@yambay.com">David MacDonald</a>
+ */
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello world";
+    }
+}
